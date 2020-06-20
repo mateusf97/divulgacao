307a8f4b2b60c9685976619cc70c5ac965d6a697
Mateus Felipe <mateus_f97@hotmail.com>
2020-06-20 20:12:44 -0300

    Add new table to migrations

--- divulgacao.api/migrations/0001.sql
+++ divulgacao.api/migrations/0001.sql
@@ -22,3 +22,29 @@ CREATE TABLE IF NOT EXISTS `user` (
 CREATE INDEX user_cpf ON user(cpf);
 CREATE INDEX user_email ON user(email);
 CREATE INDEX user_password ON user(password);
+
+
+-- @author Mateus Felipe - mateus_f97@hotmail.com
+-- This table was created to save products
+
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `url` TEXT,
+  `image_url` TEXT,
+  `title` VARCHAR(128),
+  `description` VARCHAR(1024),
+  `price` VARCHAR(128),
+  `parcel` VARCHAR(128),
+  `category_books` TINYINT(1) DEFAULT 0,
+  `category_courses` TINYINT(1) DEFAULT 0,
+  `category_subscriptions` TINYINT(1) DEFAULT 0,
+  `category_free` TINYINT(1) DEFAULT 0,
+  `is_important`tinyint(1) DEFAULT 0,
+  `clicks` INT(11) DEFAULT 0,
+  `last_modification` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE  = InnoDB;
+
+CREATE INDEX product_title ON product(title);
+CREATE INDEX product_description ON product(description);
+CREATE INDEX product_is_important ON product(is_important);
